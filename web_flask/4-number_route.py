2d4e2d81824c0f1c9db6d3963724ae8cdaa8ea2d
RahilBux <rahilbux@gmail.com>
2024-07-29 00:40:38 +0200

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Runs an app with flask
+"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """Function says hello"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Function that says HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_comp(text):
+    """Displays a message"""
+    msg = text.replace('_', ' ')
+    return "C %s" % msg
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_comp(text="is_cool"):
+    """Displays a message starting with python"""
+    msg = text.replace('_', ' ')
+    return "Python %s" % msg
+
+
+@app.route('/number/<int:n>')
+def display_int(num):
+    """Displays num is a number only if num is an integer"""
+    return "%d is a number" % num
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
