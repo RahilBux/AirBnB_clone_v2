8afaa9ee2bcc91a6cb590c601560df5bd07bc745
RahilBux <rahilbux@gmail.com>
2024-07-29 00:34:29 +0200

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Runs an app with flask
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """Function says hello"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Function that says HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_comp(text):
+    """Displays a message"""
+    msg = text.replace('_', ' ')
+    return "C %s" % msg
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.url_map.strict_slashes = False
