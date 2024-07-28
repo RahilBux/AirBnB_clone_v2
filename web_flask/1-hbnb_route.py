b2a2a1bc4d74fd23ff0f958ba2345b0d19e11840
RahilBux <rahilbux@gmail.com>
2024-07-29 00:29:36 +0200

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Runs an app with flask
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """Function says hello"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """Function that says HBNB"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.url_map.strict_slashes = False
